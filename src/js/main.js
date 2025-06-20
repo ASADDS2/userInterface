b40bc8206437ec7e9413f8a3fffc83424351f6c2
Sebastian Arnache <sarnachecantillo@gmail.com>
2025-06-20 18:27:56 -0500

    feat: beta website for training with all of CSS , HTML and JS

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,23 @@
-function userInterface(){
+
+//
+function userInterface() {
     let name = document.getElementById("name").value;
     let ageInput = document.getElementById("age").value;
 
+
+    console.log(typeof(name));
+    
     age = parseFloat(ageInput)
 
-    debugger
-    
-    if(name == "" || isNaN (age)){
-        document.getElementById("result").innerText = "Please enter a valid name or a valid age"
-    } else if ( age < 18){
+
+    if (isNaN(age)) {
+        document.getElementById("result").innerText = "Please enter a valid age"
+    } else if (name == "") {
+        document.getElementById("result").innerText = "Please enter a valid name"
+
+    } else if (age < 18) {
         document.getElementById("result").innerText = `Hi ${name}, you are underage! Keep learning and enjoying coding!`
     } else {
-        document.getElementById("result").innerText = `Hello ${name}, you are of legal age! Get ready to learn everything the world of coding has to teach you!`;
+        document.getElementById("result").innerText = `Hello ${name}, you are of legal age! Get ready to learn everything about the world of coding has to teach you!`;
     }
 };
